e2d78b1ddb28c313df88850ca2ac869ba3e7eaf0
truong <alonesera1612@gmail.com>
2025-05-01 11:07:11 +0000

    vim.cmd(w)

--- lua/fast_run/runner.lua
+++ lua/fast_run/runner.lua
@@ -1,42 +1,49 @@
 local M = {}
 
+-- Lưu danh sách custom command
 local custom_commands = {}
 
 function M.set_custom_commands(commands)
-	custom_commands = commands
+	custom_commands = commands or {}
 end
 
 function M.get_run_command()
-	local ft = vim.bo.filetype
-	local filepath = vim.fn.expand("%:p")
-	local dir = vim.fn.expand("%:p:h")
-	local filename_noext = vim.fn.expand("%:t:r")
-	local output_path = dir .. "/" .. filename_noext
+	local filetype = vim.bo.filetype
+	local fullpath = vim.fn.expand("%:p") -- Đường dẫn đầy đủ
+	local dir = vim.fn.expand("%:p:h") -- Thư mục chứa file
+	local filename_noext = vim.fn.expand("%:t:r") -- Tên file không đuôi
 
-	-- Ưu tiên dùng custom command
-	if custom_commands[ft] then
-		return custom_commands[ft](filepath)
+	-- Ưu tiên custom
+	if custom_commands[filetype] then
+		local cmd = custom_commands[filetype]
+		return string.format(cmd, fullpath, dir, filename_noext)
 	end
 
-	-- Mặc định hỗ trợ
-	if ft == "c" then
+	-- Mặc định
+	if filetype == "c" then
 		return string.format(
-			'term gcc -o "%s" "%s" -lm -lpthread -ldl -lrt && "%s"',
-			output_path,
-			filepath,
-			output_path
+			'gcc -o "%s/%s" "%s" -lm -lpthread -ldl -lrt && "%s/%s"',
+			dir,
+			filename_noext,
+			fullpath,
+			dir,
+			filename_noext
 		)
-	elseif ft == "cpp" then
-		return string.format('term g++ -o "%s" "%s" && "%s"', output_path, filepath, output_path)
-	elseif ft == "python" then
-		return 'term python3 "' .. filepath .. '"'
-	elseif ft == "java" then
-		return string.format('term javac "%s" && cd "%s" && java "%s"', filepath, dir, filename_noext)
-	elseif ft == "javascript" or ft == "js" then
-		return 'term node "' .. filepath .. '"'
+	elseif filetype == "cpp" then
+		return string.format('g++ -o "%s/%s" "%s" && "%s/%s"', dir, filename_noext, fullpath, dir, filename_noext)
+	elseif filetype == "python" then
+		return string.format('python3 "%s"', fullpath)
+	elseif filetype == "java" then
+		return string.format('javac "%s" && cd "%s" && java "%s"', fullpath, dir, filename_noext)
+	elseif filetype == "javascript" or filetype == "js" then
+		return string.format('node "%s"', fullpath)
+	elseif filetype == "go" then
+		return string.format('go run "%s"', fullpath)
+	elseif filetype == "rust" or filetype == "rs" then
+		return string.format('cargo run --manifest-path "%s/Cargo.toml"', dir)
 	end
 
-	return nil
+	return nil -- Không hỗ trợ
 end
 
 return M
