7172637eada718da20f1ccce9404463e7b88d31b
truong <alonesera1612@gmail.com>
2025-05-01 13:20:19 +0000

    created

--- lua/fast_run/init.lua
+++ lua/fast_run/init.lua
@@ -1,36 +1,64 @@
-local runner = require("fast_run.runner")
-local term = require("fast_run.term_window")
-
 local M = {}
+
+local supported_languages = {}
 
 function M.setup(opts)
 	opts = opts or {}
-	runner.set_custom_commands(opts.custom_commands or {})
+	supported_languages = {}
 
+	-- Tạo set cho các ngôn ngữ được enable
+	if opts.enable and type(opts.enable) == "table" then
+		for _, lang in ipairs(opts.enable) do
+			supported_languages[lang] = true
+		end
+	end
+
+	-- Keymap duy nhất, kiểm tra ngôn ngữ tại thời điểm nhấn phím
 	vim.keymap.set("n", "<leader>t", function()
-		-- Lấy đường dẫn file hiện tại
-		local filename = vim.api.nvim_buf_get_name(0)
+		vim.cmd("w") -- Lưu file
 
-		-- Nếu buffer chưa được lưu
-		if filename == "" then
-			vim.notify("❌ Buffer chưa được lưu thành file!", vim.log.levels.WARN)
+		local filetype = vim.bo.filetype
+
+		if not supported_languages[filetype] then
+			vim.notify("Language '" .. filetype .. "' is not enabled for run", vim.log.levels.WARN)
 			return
 		end
 
-		-- Lưu nếu có thay đổi
-		if vim.bo.modified then
-			vim.cmd("write")
-		end
+		local fullpath = vim.fn.expand("%:p")
+		local dir = vim.fn.expand("%:p:h")
+		local filename_noext = vim.fn.expand("%:t:r")
+		local output_path = string.format("%s/%s", dir, filename_noext)
+		local cmd
 
-		-- Lấy lệnh chạy tương ứng
-		local cmd = runner.get_run_command()
-		if not cmd then
-			vim.notify("⚠️ Không hỗ trợ filetype này.", vim.log.levels.WARN)
+		vim.cmd("vertical rightbelow vsplit")
+		vim.cmd("vertical resize 50")
+
+		if filetype == "c" then
+			cmd = string.format(
+				'term gcc -o "%s" "%s" -lm -lpthread -ldl -lrt && "%s"',
+				output_path,
+				fullpath,
+				output_path
+			)
+		elseif filetype == "cpp" then
+			cmd = string.format('term g++ -o "%s" "%s" && "%s"', output_path, fullpath, output_path)
+		elseif filetype == "python" then
+			cmd = string.format('term python3 "%s"', fullpath)
+		elseif filetype == "java" then
+			cmd = string.format('term javac "%s" && cd "%s" && java "%s"', fullpath, dir, filename_noext)
+		elseif filetype == "javascript" or filetype == "js" then
+			cmd = string.format('term node "%s"', fullpath)
+		else
+			print("No support file =))")
 			return
 		end
 
-		-- Mở terminal và chạy lệnh
-		term.open_and_run(cmd)
+		vim.cmd(cmd)
+		vim.cmd("startinsert")
+
+		vim.api.nvim_buf_set_keymap(0, "n", "<CR>", ":q<CR>", { noremap = true, silent = true })
+		vim.api.nvim_buf_set_keymap(0, "t", "<Up>", "<C-\\><C-n><Up>", { noremap = true, silent = true })
+		vim.api.nvim_buf_set_keymap(0, "t", "<Down>", "<C-\\><C-n><Down>", { noremap = true, silent = true })
 	end, { noremap = true, silent = true })
 end
 
